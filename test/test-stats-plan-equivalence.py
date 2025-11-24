bf222556aafa69d5791495483e6ce6d28f6257ca
Rico Bergmann <rico.bergmann1@tu-dresden.de>
2025-11-24 10:37:06 +0100

    Implement plan comparison logic for regression tests

--- test/test-stats-plan-equivalence.py
+++ test/test-stats-plan-equivalence.py
@@ -178,7 +178,20 @@ class TestFullPlanHinting(unittest.TestCase):
         self.conn.close()
 
     def assertPlansEqual(self, plan1: dict, plan2: dict, msg: str = "") -> None:
-        pass
+        if plan1["Node Type"] != plan2["Node Type"]:
+            self.fail(f"{msg}\nDifferent operators: {plan1['Node Type']} != {plan2['Node Type']}")
+
+        rel1 = _relname(plan1)
+        rel2 = _relname(plan2)
+        if rel1 != rel2:
+            self.fail(f"{msg}\nScanning different relations: {plan1} != {plan2}")
+
+        childs1 = plan1.get("Plans", [])
+        childs2 = plan2.get("Plans", [])
+        if len(childs1) != len(childs2):
+            self.fail(f"{msg}\nNumber of child plans differ: {len(childs1)} != {len(childs2)}")
+        for child1, child2 in zip(childs1, childs2):
+            self.assertPlansEqual(child1, child2, msg=msg)
 
     def test_plan_equivalence(self) -> None:
         for label, query in self.workload.items():
